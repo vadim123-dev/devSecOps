f8c5ce8b2e9b0f4425ed9b408cf8400405700306
vad <vad.has123@gmail.com>
2025-12-04 15:43:58 +0200

    Added starTriangle.py that prints stars triangle shape

---
+++ pythonLearning/sevenBoom.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+number = 0
+
+while number <= 100:
+    if number % 7 == 0 or "7" in str(number) :
+        print('boom')
+    else:
+        print(number)
+   number+=1
+
---
+++ pythonLearning/starTriangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+maxNumber=10
+i=0
+while i<10:
+  x=0
+  str='' 
+  while x<=i: 
+    str+='*' 
+    x+=1
+  print(str) 
+  i+=1 
+
+
+
