56151294e1f4ab7f857c3825ad7608891af4b414
vad <vad.has123@gmail.com>
2025-12-15 20:41:18 +0200

    added two more scripts

--- pythonLearning/multiplyTable.py
+++ pythonLearning/multiplyTable.py
@@ -1,7 +1,7 @@
 
 for col in range(1, 11):
     for row in range(1, 11):
-        num_str = str(col * row).rjust(3,' ')
+        num_str = str(col * row).rjust(4,' ')
         num_str += '|'
         print(num_str + "  ", end=' ')
     print()    
